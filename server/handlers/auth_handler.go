aaeeca6802a9951d8cd24aba350347861ae008d8
canavan-a <aidan.canavan3@gmail.com>
2023-12-26 18:17:07 -0500

    starting auth handshake for login with google rdirect works

--- server/handlers/auth_handler.go
+++ server/handlers/auth_handler.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -64,7 +68,71 @@ func ProvisionAccount(c *gin.Context) {
 
 func GenerateGoogleAuthURI(c *gin.Context) {
 
-	googleURI := fmt.Sprintf(`https://accounts.google.com/o/oauth2/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=openid+profile+email&prompt=consent`, os.Getenv("CLIENT_ID"), os.Getenv("REDIRECT_URI"))
+	clientID := os.Getenv("CLIENT_ID")
+	redirectURI := os.Getenv("REDIRECT_URI")
 
-	c.JSON(200, gin.H{"response": googleURI})
+	googleURI := fmt.Sprintf("https://accounts.google.com/o/oauth2/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=openid+profile+email&prompt=consent",
+		clientID, redirectURI)
+
+	// Gin auto escapes & valuesto unicode for some reason so we must use this bypass
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	fmt.Fprintf(c.Writer, `{"response":"%s"}`, googleURI)
+}
+
+func ValidateGoogleHandshake(c *gin.Context) {
+	var payload = struct {
+		Code string `json:"code"`
+	}{}
+
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	redirectURI := os.Getenv("REDIRECT_URI")
+
+	toSend := fmt.Sprintf(`{
+	"code": "%s",
+	"client_id": "%s",
+	"client_secret": "%s",
+	"redirect_uri": "%s",
+	"grant_type": "authorization_code"
+	}`, payload.Code, clientID, clientSecret, redirectURI)
+
+	req, err := http.NewRequest("POST", "https://oauth2.googleapis.com/token", bytes.NewBuffer([]byte(toSend)))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the order creation request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read and print the response body
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	type body struct {
+		ID string `json:"access_token"`
+	}
+
+	var accessToken body
+
+	err = json.Unmarshal(buf, &accessToken)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"response": accessToken})
+
 }
